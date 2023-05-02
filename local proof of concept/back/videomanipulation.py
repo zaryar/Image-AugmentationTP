79a7d88a7713e143bec1ebc3d758669d5f96a84f
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-05-02 19:42:48 +0200

    - added file and some really basic stuff

---
+++ local proof of concept/back/videomanipulation.py
@@ -0,0 +1,14 @@
+### in this file videos will be manipulated
+
+
+
+def read_video():
+    return video
+
+
+
+def grayscale_video():
+    #applying a grayscale video
+    return gray
+def show_video():
+
