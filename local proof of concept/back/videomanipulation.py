a1c5c3a2394daa0b6153dfa1db3e090e12e90ca8
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-05-03 16:41:09 +0200

    reading and showing videos, test video, file for basic stuff

--- local proof of concept/back/videomanipulation.py
+++ local proof of concept/back/videomanipulation.py
@@ -1,14 +1,35 @@
-### in this file videos will be manipulated
+### in this file functions for the manipulation of non- live videos will be defined
+import numpy as py
+import cv2 as cv
+
+
+
+
+
+path = "C:/Users/Valentin/Documents/Teamprojekt/Image-Augmentation/Image-AugmentationTP/local proof of concept/data/input/test_vid.mp4"
+
 
 
 
 def read_video():
-    return video
+
+    video = cv.VideoCapture(path)
+    while True:
+        isTrue, frame = video.read()
+        cv.imshow('Test', frame)
+
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
+    video.release()
+    cv.destroyAllWindows()
+
+read_video()
 
 
 
-def grayscale_video():
-    #applying a grayscale video
-    return gray
-def show_video():
 
+
+
+
+
+    
