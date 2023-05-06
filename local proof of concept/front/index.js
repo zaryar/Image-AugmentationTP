307fdef013d33abfdefc94e65316b49fb899055c
David <dawidzgoda@gmail.com>
2023-05-06 16:31:54 +0200

    added a configuration text file and added the feature to select filters over the website

--- local proof of concept/front/index.js
+++ local proof of concept/front/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const fs = require("fs");
 
 const multer = require('multer');
 const storage = multer.diskStorage({
@@ -20,11 +21,42 @@ app.get("/upload", (req, res) => {
     res.sendFile(__dirname + '/main.html');
 });
 
-app.post("/upload", upload.single('image'),(req, res) => {
+var updateData = ""
+var updateFilter = ""
+app.post("/upload", upload.single('image') ,(req, res) => {
     // res.send("Image Uploader");
     // res.status(204).send();
+    if(req.body.submit == "image"){
+         updateData = "image"
+         if(req.body.filter == "filter1")
+         {
+            updateFilter = "filter1"
+         }else if (req.body.filter == "filter2") {
+            updateFilter = "filter2"
+         }else{
+            updateFilter = "filter3"
+         }
+    }
+    if(req.body.submit == "video"){
+        updateData = "video"
+        if(req.body.filter == "filter1")
+         {
+            updateFilter = "filter1"
+         }else if (req.body.filter == "filter2") {
+            updateFilter = "filter2"
+         }else{
+            updateFilter = "filter3"
+         }
+   }
+    fs.writeFile("public/configData.txt", updateData + " ," + updateFilter , err => {
+        if(err){
+            console.err;
+            return;
+        }
+    })
     res.sendFile(__dirname + '/main.html');
   })
+
 
 app.use(express.static('public'));
 
