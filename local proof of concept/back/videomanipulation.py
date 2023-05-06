6e6b8f252e0119a352ff366d297ab0434c602c75
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-05-06 13:27:15 +0200

    grayscale works, other filters with colors should also work

--- local proof of concept/back/videomanipulation.py
+++ local proof of concept/back/videomanipulation.py
@@ -1,4 +1,6 @@
 ### in this file functions for the manipulation of non- live videos will be defined
+
+
 import numpy as py
 import cv2 as cv
 from basicpicturemanipulation import gray_filter
@@ -8,7 +10,7 @@ from basicpicturemanipulation import gray_filter
 
 
 path = "C:/Users/Valentin/Documents/Teamprojekt/Image-Augmentation/Image-AugmentationTP/local proof of concept/data/input/test_vid.mp4"
-
+filename = 'C:/Users/Valentin/Documents/Teamprojekt/Image-Augmentation/Image-AugmentationTP/local proof of concept/data/output/test_vid.avi'
 
 
 #this is a testing function for us if we are honest 
@@ -36,15 +38,17 @@ def filter_video(video_path,apply ):
     source = cv.VideoCapture(video_path)
 
     #Defining codec
-    fourcc = cv.VideoWriter_fourcc(* 'DIVX')
-    output = cv.VideoWriter('C:/Users/Valentin/Documents/Teamprojekt/Image-Augmentation/Image-AugmentationTP/local proof of concept/data/output/test_vid.avi', fourcc, 30.0, (852,  480))
+    fourcc = cv.VideoWriter_fourcc(* 'WMV1')
+    output = cv.VideoWriter(filename, fourcc, 24.0, (852,  480))
 
     while source.isOpened():
         ret, frame = source.read()
+      
         if not ret:
             print("Can't receive frame ( video has ended?). Exiting..")
             break
-        frame = apply(frame)
+        frame = cv.resize(frame, (852,480))
+        #frame = apply(frame)
 
         #actual writing
         output.write(frame)
@@ -58,8 +62,36 @@ cv.destroyAllWindows()
 
 
 
+def grayscale_video(video_path):
+    source = cv.VideoCapture(video_path)
 
-filter_video(path, gray_filter)
+    #Defining codec
+    fourcc = cv.VideoWriter_fourcc(* 'WMV1') 
+    output = cv.VideoWriter(filename, fourcc, 24.0, (852,  480),)
+
+    while source.isOpened():
+        ret, frame = source.read()
+      
+        if not ret:
+            print("Can't receive frame ( video has ended?). Exiting..")
+            break
+        frame = cv.resize(frame, (852,480))
+        frame = gray_filter(frame)
+        frame = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+        #actual writing
+        output.write(frame)
+
+        cv.imshow('frame',frame)
+        if cv.waitKey(1)== ord("d"):
+            break
+    source.release()
+    output.release()
+cv.destroyAllWindows()
+
+
+#filter_video(path, gray_filter)
+
+grayscale_video(path)
 
 
     
