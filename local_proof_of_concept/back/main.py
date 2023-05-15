fc38d381ab16e1e62b9876de87a3fb8fd6bb40a3
Bela Thraen <bela.thraen.bt@gmail.com>
2023-05-15 10:48:27 +0200

    read config file as txt and added python loop to detect action. ATTENTION: renamed folder local\ proof\ of\ concept to local_proof_of_concept

---
+++ local_proof_of_concept/back/main.py
@@ -0,0 +1,22 @@
+from filters import *
+from videomanipulation import filter_video
+import os
+
+
+PATH = "./local_proof_of_concept/front/public/images/input/test_vid.mp4"
+FILENAME = './local_proof_of_concept/front/public/images/output/test_vid.mp4'
+
+#Translation Test Dictionary
+
+dict = {"filter1": filter_blurred , "filter2" : filter_flip, "filter3" : filter_pixel }
+
+#READ CONFIG FILE
+while True:
+    file_exists = os.path.exists("./local_proof_of_concept/front/public/configData.txt")
+    if file_exists:
+        f = open("./local_proof_of_concept/front/public/configData.txt","r")
+        format = f.readline()
+        filter = f.readline()
+        os.remove("./local_proof_of_concept/front/public/configData.txt")
+        filter_video(PATH, dict["filter1"], FILENAME)
+
