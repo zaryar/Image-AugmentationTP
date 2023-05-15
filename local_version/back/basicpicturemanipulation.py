9f1aa0f39e6fd2577599d445cc3e864736d26448
zaryar <eichlermartin37@gmail.com>
2023-05-15 19:14:15 +0200

    moved everything in local_version

---
+++ local_version/back/basicpicturemanipulation.py
@@ -0,0 +1,57 @@
+#basic functions for the manipulation of pictures are done here
+
+import cv2 as cv
+import numpy as np
+import os 
+from filters import filter_bw
+
+test = "Test.jpeg"
+def get_path(folder, image):
+    #gets the path to the image and hopefully working on *nix and Windows
+    #folder is the name of the folder in data the image is saved in
+    #image is the name of the image
+
+    #getting the current directory
+    cur_dir = os.getcwd()
+    #going up a directory
+    os.chdir("..") 
+    #constucting the path 
+    path = os.path.join(os.getcwd(),'data',folder, image)
+
+    #going back to the original directory
+    os.chdir(cur_dir)
+   
+    return path
+
+
+def read_picture():
+        #getting the right path for the machine
+        path = get_path("input", test)
+
+       
+
+        img = cv.imread(path)
+
+        return img
+
+def gray_filter(img):
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+        return gray
+
+def show_image(img, gray):
+        cv.imshow('Original image',img)
+        cv.imshow('Gray image', gray)
+
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+def write_image(gray):
+        cv.imwrite(get_path("output",test), gray)
+
+def image_filter(path, apply, filename):
+        img = cv.imread(path)
+        img = apply(img)
+        cv.imwrite(filename, img)
+
+image_filter("local proof of concept/data/input/test.jpg", filter_bw, "local proof of concept/data/output/test.jpg")
---
+++ local_version/front/index.js
@@ -0,0 +1,75 @@
+//library
+const express = require("express");
+const app = express();
+const path = require('path');
+const fs = require("fs");
+//
+
+app.use(express.static('public'));  /* tells expressJS where to find css and js files */
+
+// function that gives the name to the new files added to input
+var i = 0;
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images/input')
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        // cb(null, Date.now() + path.extname(file.originalname))
+        if (path.extname(file.originalname).length > 0) {
+            cb(null, "image" + path.extname(file.originalname))
+        } else {
+            cb(null, "frame" + i + ".png")
+            i++;
+            if(i > 25){
+                i = 0
+            }
+
+        }
+    }
+})
+
+const upload = multer({ storage: storage }) //function to save the image sent
+
+app.get("/upload", (req, res) => {
+    res.sendFile(__dirname + '/main.html'); //function send the user to main.html when they open the webpage
+});
+
+// Gets called when a post request is send 
+
+var updateData = ""
+var updateFilter = ""
+app.post("/upload", upload.single('image'), (req, res) => {
+    // res.status(204).send();
+    //update configData
+    if (req.body.submit == "image" || req.body.submit == "snapshot" || req.body.submit == "stream" || req.body.submit == "video") {
+        updateData = req.body.submit
+        if (req.body.filter == "filter1") {
+            updateFilter = "filter1"
+        } else if (req.body.filter == "filter2") {
+            updateFilter = "filter2"
+        } else if (req.body.filter == "filter3"){
+            updateFilter = "filter3"
+        }else{
+            updateFilter = "none"
+        }
+    }
+    fs.writeFile("public/configData.txt", updateData + " ," + updateFilter, err => {
+        if (err) {
+            console.err;
+            return;
+        }
+    })
+    if (updateData != "stream") {
+        res.sendFile(__dirname + '/main.html');
+    } else {
+        // In case of a stream, we donÇt want the website to reload
+        res.send(null)
+    }
+})
+
+app.use(express.static('public'));
+
+app.listen(3001);
+console.log("3001 is the port");
