0c4a251dffc806060fff494563799b2580c4e2b0
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-06-28 20:13:48 +0200

    removed the old style transfer

--- local_version/back/fast_neural_style/neural_style/neural_style_test.py
+++
@@ -1,4 +0,0 @@
-import neural_style
-
-
-neural_style.filter_monet("local_version/back/fast_neural_style/images/content-images/vaile.png","local_version/back/fast_neural_style/images/output-images/mein_vaile.png")
