fc4330ec0f14dbd3e7375c900cc400415daca8c4
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-07-01 13:41:41 +0200

    tried a more basic approach

--- local_version/back/fast-ns/experiments/filters_for_images.py
+++ local_version/back/fast-ns/experiments/filters_for_images.py
@@ -64,8 +64,36 @@ def stylize_image(image_path,style_model,ouput_path):
 
 
 
-    #Test 
-    model = load_models("starry_night")
-    stylize_image("local_version/back/fast-ns/experiments/images/content/flowers.jpg",model,"local_version/back/fast-ns/experiments/test.jpg")
+    def evaluate(content_path, style_path, output_path):
+        cuda = True
+        content_image = utils.tensor_load_rgbimage(content_path, size=512, keep_asp=True)
+        content_image = content_image.unsqueeze(0)
+        style = utils.tensor_load_rgbimage(style_path, size=512)
+        style = style.unsqueeze(0)    
+        style = utils.preprocess_batch(style)
+
+        style_model = Net(ngf=128)
+        model_dict = torch.load("local_version/back/fast-ns/experiments/models/21styles.model")
+        model_dict_clone = model_dict.copy()
+        for key, value in model_dict_clone.items():
+            if key.endswith(('running_mean', 'running_var')):
+                del model_dict[key]
+        style_model.load_state_dict(model_dict, False)
+
+        if cuda:
+            style_model.cuda()
+            content_image = content_image.cuda()
+            style = style.cuda()
+
+        style_v = Variable(style)
+
+        content_image = Variable(utils.preprocess_batch(content_image))
+        style_model.setTarget(style_v)
+
+        output = style_model(content_image)
+        #output = utils.color_match(output, style_v)
+        utils.tensor_save_bgrimage(output.data[0], output_path, cuda)
+
+    evaluate("local_version/back/fast-ns/experiments/images/content/flowers.jpg","local_version/back/fast-ns/experiments/images/9styles/candy.jpg","local_version/back/fast-ns/experiments/output.jpg")
 			
 	
