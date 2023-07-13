108b69a65bb10ab145ef5843b3f85bdd05be3e91
JoJoBarthold2 <valentin.j.schmidt@web.de>
2023-07-13 20:28:18 +0200

    removed superflous show video function

--- local_version/back/videomanipulation.py
+++ local_version/back/videomanipulation.py
@@ -12,26 +12,10 @@ import cv2 as cv
 PATH = "./local_version/data/input/test_vid.mp4"
 FILENAME = './local_version/data/output/test_vid.avi'
 CODEC = 'WMV1'
-ASPECTPATH = "./local_version/data/input/aspect_test.mp4" #we can use that to show that the format think works
+ASPECTPATH = "./local_version/data/input/aspect_test.mp4" #we can use that to show that the format thing works
 
 #this is a testing function for us if we are honest 
-def show_video(video_path):
 
-    video = cv.VideoCapture(video_path)
-    while video.isOpened():
-        ret, frame = video.read()
-        cv.imshow('Test', frame)
-
-        if not ret:
-            print("show's over")
-            break
-
-
-
-        if cv.waitKey(20) & 0xFF==ord('d'):
-            break
-    video.release()
-    cv.destroyAllWindows()
 
 
 # this function takes a path and a filter and applies it to the video in the path, then saves the video as an .avi / only works for pictures with all color channels availabl
